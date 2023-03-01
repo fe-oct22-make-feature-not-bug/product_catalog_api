cd7b9e1a978977a0a01179bc889cb00a066ac4c0
VladimirEdelmann <VladimirEdelmann@gmail.com>
2023-03-02 01:32:07 +0200

    Add libs, run prettir, add configs

--- models/phone.js
+++ models/phone.js
@@ -1,8 +1,6 @@
 'use strict';
 
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Phone extends Model {
     /**
@@ -10,33 +8,36 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate(/*models*/) {
       // define association here
     }
   }
-  Phone.init({
-    name: DataTypes.STRING,
-    namespaceId: DataTypes.STRING,
-    capacityAvailable: DataTypes.ARRAY(DataTypes.STRING),
-    capacity: DataTypes.STRING,
-    priceRegular: DataTypes.FLOAT,
-    priceDiscount: DataTypes.FLOAT,
-    colorsAvailable: DataTypes.ARRAY(DataTypes.STRING),
-    color: DataTypes.STRING,
-    images: DataTypes.ARRAY(DataTypes.STRING),
-    description: DataTypes.JSONB,
-    screen: DataTypes.STRING,
-    resolution: DataTypes.STRING,
-    processor: DataTypes.STRING,
-    ram: DataTypes.STRING,
-    camera: DataTypes.STRING,
-    zoom: DataTypes.STRING,
-    cell: DataTypes.ARRAY(DataTypes.STRING)
-  }, {
-    sequelize,
-    modelName: 'Phone',
-    createdAt: true,
-    updatedAt: false
-  });
+  Phone.init(
+    {
+      name: DataTypes.STRING,
+      namespaceId: DataTypes.STRING,
+      capacityAvailable: DataTypes.ARRAY(DataTypes.STRING),
+      capacity: DataTypes.STRING,
+      priceRegular: DataTypes.FLOAT,
+      priceDiscount: DataTypes.FLOAT,
+      colorsAvailable: DataTypes.ARRAY(DataTypes.STRING),
+      color: DataTypes.STRING,
+      images: DataTypes.ARRAY(DataTypes.STRING),
+      description: DataTypes.JSONB,
+      screen: DataTypes.STRING,
+      resolution: DataTypes.STRING,
+      processor: DataTypes.STRING,
+      ram: DataTypes.STRING,
+      camera: DataTypes.STRING,
+      zoom: DataTypes.STRING,
+      cell: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    {
+      sequelize,
+      modelName: 'Phone',
+      createdAt: true,
+      updatedAt: false,
+    }
+  );
   return Phone;
 };
