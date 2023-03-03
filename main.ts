f675bc24bc63e176c4d64e6ba95d330ab4847af9
Volodymyr Hotsaniuk <VladimirEdelmann@gmail.com>
2023-03-03 11:35:26 +0200

    Correct port

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 import { createServer } from './src/createServer';
 
 const server = createServer();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
   console.log(`Server is running on localhost:${port}`);
