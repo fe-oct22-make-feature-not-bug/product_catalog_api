7607d90e47c82bbe3a361782912c74dd9a416a31
VladimirEdelmann <VladimirEdelmann@gmail.com>
2023-03-07 17:14:07 +0200

    Add /newPhones and /hotPrices endpoints

--- src/controllers/phones.js
+++ src/controllers/phones.js
@@ -3,13 +3,10 @@
 const phonesService = require('../services/phones');
 
 const getAll = (req, res) => {
-  phonesService
-    .getAll()
-    .then((phones) => {
-      res.send(phones);
+  phonesService.getAll().then((phones) => {
+    res.send(phones);
   });
 };
-
 
 const getOne = (req, res) => {
   const { phoneId } = req.params;
@@ -19,20 +16,30 @@ const getOne = (req, res) => {
       if (!foundPhone) {
         res.sendStatus(404);
 
-      return;
-    }
+        return;
+      }
 
-    res.send(foundPhone);
+      res.send(foundPhone);
 
-    return foundPhone;
-  })
-  .catch((err) => {
-    console.error(err);
-    res.sendStatus(500);
-  });
+      return foundPhone;
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const getNew = (req, res) => {
+  phonesService.getNew().then((phones) => res.send(phones));
+};
+
+const getHot = (req, res) => {
+  phonesService.getHot().then((phones) => res.send(phones));
 };
 
 module.exports = {
   getAll,
   getOne,
+  getNew,
+  getHot,
 };
--- src/services/phones.js
+++ src/services/phones.js
@@ -1,7 +1,7 @@
 'use strict';
 
+const { Sequelize } = require('sequelize');
 const { Phone } = require('../../models');
-
 
 function getAll() {
   return Phone.findAll({
@@ -13,7 +13,23 @@ function findById(phoneId) {
   return Phone.findByPk(phoneId);
 }
 
+function getNew() {
+  return Phone.findAll({
+    order: ['createdAt'],
+    limit: 10,
+  });
+}
+
+function getHot() {
+  return Phone.findAll({
+    order: Sequelize.literal('("priceRegular" - "priceDiscount") DESC'),
+    limit: 10,
+  });
+}
+
 module.exports = {
   getAll,
   findById,
+  getNew,
+  getHot,
 };
