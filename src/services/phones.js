a24cbfb2e7583c045283b7f2c41524f0d5870f3e
VladimirEdelmann <VladimirEdelmann@gmail.com>
2023-03-10 10:39:41 +0200

    revert

--- src/services/phones.js
+++ src/services/phones.js
@@ -3,23 +3,29 @@
 const { Sequelize } = require('sequelize');
 const { ShortPhones } = require('../../models');
 
-async function getAll(pageNumber, pageItems) {
-  const offset = (pageNumber - 1) * pageItems;
+// async function getAll(pageNumber, pageItems) {
+//   const offset = (pageNumber - 1) * pageItems;
 
-  const totalCount = await ShortPhones.count();
+//   const totalCount = await ShortPhones.count();
 
-  const items = await ShortPhones.findAll({
+//   const items = await ShortPhones.findAll({
+//     order: ['createdAt'],
+//     limit: pageItems,
+//     offset: offset,
+//   });
+
+//   const totalPages = Math.ceil(totalCount / pageItems);
+
+//   return {
+//     items,
+//     totalPages,
+//   };
+// }
+
+function getAll() {
+  return ShortPhones.findAll({
     order: ['createdAt'],
-    limit: pageItems,
-    offset: offset,
   });
-
-  const totalPages = Math.ceil(totalCount / pageItems);
-
-  return {
-    items,
-    totalPages,
-  };
 }
 
 function findById(phoneId) {
