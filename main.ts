fe89a815c8ed0c9c8feee5169f006035011e0ebf
VladimirEdelmann <VladimirEdelmann@gmail.com>
2023-03-10 03:41:01 +0200

    Update pagination

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 import { createServer } from './src/createServer';
 
 const server = createServer();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log(`Server is running on localhost:${port}`);
